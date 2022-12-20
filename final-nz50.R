9d89fce120a654621337df3224f854f779ce6c83
SharonLiu716 <87752902+SharonLiu716@users.noreply.github.com>
2022-12-20 16:53:54 +0800

    Garch fit

--- final-nz50.R
+++ final-nz50.R
@@ -1,3 +1,5 @@
+rm(list=ls(all=TRUE)) 
+setwd("C:/Users/User/Documents/Time-Series-collaboration")
 library(zoo)
 library(xts)
 library(TTR)
@@ -9,47 +11,7 @@ library(qpcR)
 library(fpp2)
 library(fUnitRoots)
 library(stats)
-#紐西蘭50
-getSymbols("^NZ50", from ='2012-12-12', to = '2022-12-12')
-NZ50 <- as.xts(data.frame(NZ50 = NZ50[, "NZ50.Adjusted"]))
-NZ50 <- na.omit(NZ50)
-names(NZ50) = c("NZ50")
-index(NZ50) = as.Date(index(NZ50))
-logrt.NZ50 <- apply( log(NZ50) , 2 , diff )*100
-plot(NZ50,main=names(NZ50))
-acf(NZ50, lag.max = 30)
-pacf(NZ50, lag.max = 30)
-#plot(logrt.NZ50,main=names(logrt.NZ50))
-tp.NZ50=tidy(as.xts(logrt.NZ50)) %>%
-  ggplot(aes(x=index,y=value)) + geom_line(color="#E7B800") +geom_hline(yintercept = 0,color='black')+
-  labs(title = paste(colnames(logrt.NZ50),"Daily Return for 10 years"),subtitle = "Daily Return",caption = " Time Plot") +
-  xlab("Date") + ylab("Daily Return in Percentage") 
-tp.NZ50
-acf(logrt.NZ50, lag.max = 30)
-pacf(logrt.NZ50, lag.max = 30)
-Box.test(logrt.NZ50, lag=10, type='Ljung')
-
-getSymbols("USDNZD=X", from = '2012-10-12', to = '2022-10-12')
-USDNZD<-as.xts(data.frame(`USDNZD=X` = `USDNZD=X`[, paste0("USDNZD=X.Adjusted")]))
-USDNZD<-na.omit(USDNZD)
-names(USDNZD) = c("USDNZD")
-index(USDNZD) = as.Date(index(USDNZD))
-logrt.USDNZD<-apply( log(USDNZD) , 2 , diff )*100
-plot(USDNZD,main=names(USDNZD))
-acf(USDNZD, lag.max = 30)
-pacf(USDNZD, lag.max = 30)
-#plot(logrt.USDNZD,main=names(logrt.USDNZD))
-tp.USDNZD=tidy(as.xts(logrt.USDNZD)) %>%
-  ggplot(aes(x=index,y=value)) + geom_line(color="#E7B800") +geom_hline(yintercept = 0,color='black')+
-  labs(title = paste(colnames(logrt.USDNZD),"Daily Return for 10 years"),subtitle = "Daily Return",caption = " Time Plot") +
-  xlab("Date") + ylab("Daily Return in Percentage") 
-tp.USDNZD
-acf(logrt.USDNZD, lag.max = 30)
-pacf(logrt.USDNZD, lag.max = 30)
-Box.test(logrt.USDNZD, lag=10, type='Ljung')
-
-
-# Check aic and it's order given max.p & max.q
+library(fGarch)
 detect <- function(data,p,q){
   temp <- c()
   ord <- c()
@@ -62,24 +24,76 @@ detect <- function(data,p,q){
   }
   list(likelihood = sort(temp),order = ord[order(temp), ])
 }
-res <- function(data){
+res <- function(data,obj){
   
-  acf(data, lag.max = 30)
-  #  title(paste("ACF","of",name), line = 0)
-  
-  pacf(data, lag.max = 30)
-  #  title(paste("PACF","of",name), line = 0.3)
+  ACF<-acf(data, lag.max = 30,plot = FALSE)
+  plot(ACF, main = paste("ACF of",obj))
+  PACF<-pacf(data, lag.max = 30,plot = FALSE)
+  plot(PACF, main = paste("PACF of",obj))
+  par(mfrow=c(1,1))
+  plot(as.xts(data),main= paste("Time Plot of",obj))
   
 }
-detect(logrt.NZ50, p = 9, q = 2)
+#美紐匯率
+getSymbols("USDNZD=X", from = '2012-10-12', to = '2022-10-12')
+USDNZD<-as.xts(data.frame(`USDNZD=X` = `USDNZD=X`[, paste0("USDNZD=X.Adjusted")]))
+USDNZD<-na.omit(USDNZD)
+names(USDNZD) = c("USDNZD")
+index(USDNZD) = as.Date(index(USDNZD))
+res(USDNZD,'original USDNZD')
+
+
+logrt.USDNZD<-apply( log(USDNZD) , 2 , diff )*100
+res(logrt.USDNZD,'log return of USDNZD')
+
+Box.test(logrt.USDNZD, lag=10, type='Ljung')
+
+
+#紐西蘭50
+getSymbols("^NZ50", from ='2012-12-12', to = '2022-12-12')
+NZ50 <- as.xts(data.frame(NZ50 = NZ50[, "NZ50.Adjusted"]))
+NZ50 <- na.omit(NZ50)
+names(NZ50) = c("NZ50")
+index(NZ50) = as.Date(index(NZ50))
+res(NZ50,'original NZ50')
+
+logrt.NZ50 <- apply( log(NZ50) , 2 , diff )*100
+res(logrt.NZ50,'log return of NZ50')
+
+Box.test(logrt.NZ50, lag=10, type='Ljung')
+
+
+#adf test , p-value<0.05->don't diff
 adfTest(logrt.NZ50,type = "ct")
+
+# Check aic and it's order given max.p & max.q
+detect(logrt.NZ50, p = 9, q = 3)
+
 fit.NZ50 <- arima(logrt.NZ50, order = c(4,0,2))
 fit.NZ50
+Residual<-fit.NZ50$residuals
+res(Residual,'Residual of ARIMA(4,0,2)for log NZ50')
+Box.test(Residual, lag = 12, type='Ljung')
+res(Residual^2,'Residual^2 of ARIMA(4,0,2)for log NZ50')
+Box.test(Residual^2, lag = 12, type='Ljung')
+#garch(1,0) residual box test result:p-value = 0.01085-->garch(1,1)
+m5 = garchFit(logrt.NZ50~arma(4,2) + garch(1,1), data = logrt.NZ50, trace = FALSE)
+summary(m5)
+stresi = residuals(m5, standardize = T)
+res(stresi,'Residual of ARMA(4,2)+Garch(1,1) for log NZ50')
+Box.test(stresi, lag = 12, type='Ljung')
+res(stresi^2,'Residual^2 of ARMA(4,2)+Garch(1,1) for log NZ50')
+Box.test(stresi^2, lag = 12, type='Ljung')
+
+
+
+
+
 fit.NZ50 <- arima(logrt.NZ50, order = c(4,0,2),
                   fixed = c(NA,NA,0,NA,NA,NA,NA),transform.pars= FALSE)
 fit.NZ50
 
-fit.NZ50$residuals %>% res()
+
 tsdiag(fit.NZ50)
 Box.test(fit.NZ50$residuals, lag=10, type='Ljung')
 arch.test(fit.NZ50)
